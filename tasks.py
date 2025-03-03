abc6e71ec761d525349e10db16f7be0170272058
Neon1rs <lex40535@gmail.com>
2025-03-03 22:28:15 +0300

    Update FULL_TASK configuration with expanded module options and skip mechanisms
    - Enhance FULL_TASK with more diverse and flexible module execution paths
    - Add multiple variations of module combinations with optional 'skip' modules
    - Introduce more randomization and fallback options in task execution
    - Expand task list with new task configurations like STAKING_TASK, EXCHANGE_TASK, and bridging tasks
    - Add multilingual comments explaining task configuration syntax and usage

--- tasks.py
+++ tasks.py
@@ -2,7 +2,52 @@ TASKS = [
     "FULL_TASK",
 ]
 
+"""
+EN:
+You can create your own task with the modules you need 
+and add it to the TASKS list or use our ready-made preset tasks.
 
+( ) - Means that all of the modules inside the brackets will be executed 
+in random order
+[ ] - Means that only one of the modules inside the brackets will be executed 
+on random
+SEE THE EXAMPLE BELOW:
+
+RU:
+Вы можете создать свою задачу с модулями, которые вам нужны, 
+и добавить ее в список TASKS, см. пример ниже:
+
+( ) - означает, что все модули внутри скобок будут выполнены в случайном порядке
+[ ] - означает, что будет выполнен только один из модулей внутри скобок в случайном порядке
+СМОТРИТЕ ПРИМЕР НИЖЕ:
+
+CHINESE:
+你可以创建自己的任务，使用你需要的模块，
+并将其添加到TASKS列表中，请参见下面的示例：
+
+( ) - 表示括号内的所有模块将按随机顺序执行
+[ ] - 表示括号内的模块将按随机顺序执行
+
+--------------------------------
+!!! IMPORTANT !!!
+EXAMPLE | ПРИМЕР | 示例:
+
+TASKS = [
+    "CREATE_YOUR_OWN_TASK",
+]
+CREATE_YOUR_OWN_TASK = [
+    "memebridge",
+    ("apriori", "magma", "shmonad"),
+    ["ambient", "izumi", "bean"],
+    "collect_all_to_monad",
+]
+--------------------------------
+
+
+BELOW ARE THE READY-MADE TASKS THAT YOU CAN USE:
+СНИЗУ ПРИВЕДЕНЫ ГОТОВЫЕ ПРИМЕРЫ ЗАДАЧ, КОТОРЫЕ ВЫ МОЖЕТЕ ИСПОЛЬЗОВАТЬ:
+以下是您可以使用的现成任务：
+"""
 FAUCET = [
     "faucet",
 ]
@@ -15,11 +60,21 @@ BRIDGE_AND_SWAPS = [
 
 
 FULL_TASK = [
-    ("izumi", "aircraft", "lilchogstars", "bean", "swaps"),
-    ["ambient", "izumi", "bean"],
-    ["apriori", "magma", "shmonad"],
-    ["magiceden", "monadking"],
+    ["izumi", "swaps", "ambient", "bean", "skip"],
+    ["izumi", "swaps", "ambient", "bean", "skip", "skip", "skip"],
+    ["izumi", "swaps", "ambient", "bean", "skip", "skip", "skip"],
+    ["izumi", "aircraft", "lilchogstars", "bean", "swaps", "skip"],
+    ["ambient", "izumi", "bean", "skip", "skip"],
+    "collect_all_to_monad"
+    ["apriori", "magma", "shmonad", "kintsu", "skip", "skip"],
+    ["apriori", "magma", "shmonad", "kintsu", "skip"],
+    ["ambient", "izumi", "bean", "magiceden", "monadking", "skip"],
+    ["magiceden", "monadking", "aircraft", "skip", "skip"],
     "collect_all_to_monad",
+    ["ambient", "izumi", "bean", "magiceden", "monadking", "skip"],
+    ["izumi", "swaps", "ambient", "bean", "skip", "skip", "skip"],
+    ["owlto", "skip", "skip"],
+    ["izumi", "swaps", "ambient", "bean", "skip", "skip"],
     "logs",
 ]
 
@@ -29,8 +84,25 @@ BRIDGE_SEPOLIA_AND_CONVERT_TO_MON = [
     "collect_all_to_monad",
 ]
 
-CUSTOM_TASK = [
-    "",
+STAKING_TASK = [
+    ("apriori", "magma", "shmonad", "kintsu"),
+]
+
+EXCHANGE_TASK = [
+    "cex_withdrawal",
+]
+
+EXCHANGE_AND_TESTNET_BRIDGE_TASK = [
+    "cex_withdrawal",
+    "testnet_bridge",
+    "orbiter",
+    "collect_all_to_monad",
+]
+
+EXCHANGE_AND_MEMEBRIDGE_TASK = [
+    "cex_withdrawal",
+    "memebridge",
+    "collect_all_to_monad",
 ]
 
 # FAUCETS
