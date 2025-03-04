46a56dd2b545d6f374a10ef75b0327ee5157ca4a
ZeinDeRikko <129055694+ZeinDeRikko@users.noreply.github.com>
2025-03-04 09:16:56 +0200

    Fixed aiodns

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from loguru import logger
 import urllib3
 import sys
 import asyncio
-
-from process import start
-
-import asyncio
 import platform
 
-# if platform.system() == "Windows":
-#     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+from process import start
+import src
+
+
+
+# SETTING POLICY FOR WINDOWS
+config = src.utils.get_config()
+using_playwright = 'faucet' in str(config.FLOW.TASKS) or 'dusted' in str(config.FLOW.TASKS)
+
+
+if not using_playwright:
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 async def main():
