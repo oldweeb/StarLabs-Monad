42908a1d374c3a6a30791275dd0224efba200ede
ZeinDeRikko <129055694+ZeinDeRikko@users.noreply.github.com>
2025-03-19 11:57:47 +0200

    Faucet update

--- src/model/monad_xyz/faucet.py
+++ src/model/monad_xyz/faucet.py
@@ -29,6 +29,33 @@ async def faucet(
             user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
             href = "https://testnet.monad.xyz/"
 
+            headers = {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "accept-language": "en-GB,en;q=0.9",
+                "priority": "u=0, i",
+                "sec-ch-ua": '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "none",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+            }
+
+            response = await session.get("https://testnet.monad.xyz/", headers=headers)
+
+            # Извлекаем token и timestamp через split
+            x_verification_token = response.text.split(
+                'requestVerification\\":{\\"token\\":\\"'
+            )[1].split('\\"')[0]
+            x_timestamp = response.text.split('timestamp\\":\\"')[1].split('\\"')[0]
+
+            logger.info(
+                f"[{account_index}] | Got verification token: {x_verification_token}"
+            )
+
             # # First get the Vercel challenge token
             # response = await session.post(
             #     "http://api.nocaptcha.io/api/wanda/vercel/universal",
@@ -150,6 +177,8 @@ async def faucet(
                 "referer": "https://testnet.monad.xyz/",
                 "accept-language": "en-GB,en;q=0.9",
                 "priority": "u=1, i",
+                "x-request-timestamp": x_timestamp,
+                "x-request-verification-token": x_verification_token,
             }
 
             # wanda_result = await make_wanda_request(
